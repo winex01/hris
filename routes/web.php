30ce6aa7f4d1cf1f69366aea972352408f6c96a2
winex01 <winnie131212592@gmail.com>
2020-10-22 16:16:10 +0800

    clean web route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-	return redirect()->route('home');
+	return redirect()->route('backpack.auth.login');
 });
 
-Route::get('/home', function () {
-    return redirect()->route('backpack.dashboard');
-})->name('home');
