70cb4036109910068ceef14851377939907ffb71
winex01 <winnie131212592@gmail.com>
2020-10-22 13:30:00 +0800

    fix backpack sometimes redirect to home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('backpack.dashboard');
+	return redirect()->route('home');
 });
+
+Route::get('/home', function () {
+    return redirect()->route('backpack.dashboard');
+})->name('home');
