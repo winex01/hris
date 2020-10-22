77082a8489bc2dec099739a7f282be0c1d8303eb
winex01 <winnie131212592@gmail.com>
2020-10-23 01:04:42 +0800

    add TODO comment

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -6,6 +6,7 @@
 </li>
 
 <!-- Users, Roles, Permissions -->
+{{-- TODO: hide nav authentication no authority in any of the ff. users, roles, permission VIEW --}}
 <li class="nav-item nav-dropdown">
 	<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
 	<ul class="nav-dropdown-items">
@@ -16,4 +17,11 @@
 	  <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
 	</ul>
 </li>
+
+
+@php
+	// TODO: Roles
+	// TODO: Permission
+@endphp
+
 
