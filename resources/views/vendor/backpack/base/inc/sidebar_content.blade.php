daa7723b668c5a3f8932a93109a355aef3e65c06
winex01 <winnie131212592@gmail.com>
2020-10-23 20:35:58 +0800

    use trans language

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,26 +1,26 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item">
 	<a class="nav-link" href="{{ backpack_url('dashboard') }}">
-		<i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}
+		<i class="la la-home nav-icon"></i> @lang('lang.dashboard')
 	</a>
 </li>
 
 <!-- Users, Roles, Permissions -->
 @canany(['user_view', 'role_view', 'permission_view'])
 	<li class="nav-item nav-dropdown">
-		<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+		<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> @lang('lang.authentication')</a>
 		<ul class="nav-dropdown-items">
 
 			@can('user_view')
-			 	<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
+			 	<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>@lang('lang.users')</span></a></li>
 			@endcan
 
 			@can('role_view')
-			  	<li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+			  	<li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>@lang('lang.roles')</span></a></li>
 			@endcan
 
 			@can('permission_view')
-				<li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+				<li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>@lang('lang.permissions')</span></a></li>
 			@endcan
 
 		</ul>
@@ -29,6 +29,7 @@
 
 @php
 	// TODO:: permissions and roles seeder
+	// TODO:: use language trans
 @endphp
 
 
