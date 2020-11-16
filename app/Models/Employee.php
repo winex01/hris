76d76ee678362796590b57c7964979fa80d48383
winex01 <winnie131212592@gmail.com>
2020-11-16 19:00:17 +0800

    separate image access mutators and relationship as trait

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use CrudTrait;
+    use \App\Traits\ImageTrait;
 
     /*
     |--------------------------------------------------------------------------
@@ -28,15 +29,6 @@ class Employee extends Model
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
-    private function deleteImage($attribute_name, $disk = 'public')
-    {
-        if ($this->image) {
-            // remove storage/ from img path
-            $imgPath = str_replace('storage/', '', $this->{$attribute_name});
-            \Storage::disk($disk)->delete($imgPath);
-            $this->image->delete();
-        }
-    }
 
     /*
     |--------------------------------------------------------------------------
@@ -63,63 +55,11 @@ class Employee extends Model
     | ACCESSORS
     |--------------------------------------------------------------------------
     */
-    public function getImgUrlAttribute()
-    {
-        if (!$this->image) {
-            return null; //TODO:: add default photo
-        }
-        return 'storage/'.$this->image->url;
-    }
 
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
     |--------------------------------------------------------------------------
     */
-    public function setImgAttribute($value, $data = [
-        'attribute_name' => 'img_url', //getPhotoUrlAttribute()  
-        'destination_path' => 'images/photo',   
-        'disk' => 'public',   
-    ]){
-        $attribute_name = $data['attribute_name']; 
-        $destination_path = $data['destination_path']; 
-        $disk = $data['disk'];
-
-        // if the image was erased
-        if ($value==null) {
-            // delete the image from disk
-            $this->deleteImage($attribute_name);
-        }
-
-        // if a base64 was sent, store it in the db
-        if (\Str::startsWith($value, 'data:image'))
-        {
-            // 0. Make the image
-            $image = \Image::make($value)->encode('jpg', 90);
-
-            // 1. Generate a filename.
-            $filename = md5($value.time()).'.jpg';
-
-            // 2. Store the image on disk.
-            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
-
-            // 3. Delete the previous image, if there was one.
-            $this->deleteImage($attribute_name);
-
-            // 4. Save the public path to the database
-            // but first, remove "public/" from the path, since we're pointing to it 
-            // from the root folder; that way, what gets saved in the db
-            // is the public URL (everything that comes after the domain name)
-            $public_destination_path = \Str::replaceFirst('public/', '', $destination_path);
-            // $this->attributes[$attribute_name] = $public_destination_path.'/'.$filename;
-            
-            $this->image()->save(
-                new \App\Models\Image([
-                    'url' => $public_destination_path.'/'.$filename
-                ])
-            );
-        }
-
-    }
 
 }
