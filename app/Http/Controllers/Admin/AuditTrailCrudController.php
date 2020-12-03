0fdbada0d1faa63b998c112357f6390cfc9f441b
winex01 <winnie131212592@gmail.com>
2020-12-03 13:49:27 +0800

    add other operation

--- app/Http/Controllers/Admin/AuditTrailCrudController.php
+++ app/Http/Controllers/Admin/AuditTrailCrudController.php
@@ -14,6 +14,9 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 class AuditTrailCrudController extends CrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
     use \App\Http\Controllers\Admin\Traits\CrudExtendTrait;
 
