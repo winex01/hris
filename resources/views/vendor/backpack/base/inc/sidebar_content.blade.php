6229bbd2068c5cf876e06d42bedd0ec3dda71a1a
winex01 <winnie131212592@gmail.com>
2020-12-08 11:03:06 +0800

    refactor and add permissions list route to check auth permission

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -163,9 +163,10 @@
 
 {{-- 
 	TODO:: app settings seeders
-	TODO:: add revision
+    TODO:: add revision user
     TODO:: add revision roles
     TODO:: add revision permission
+    TODO:: add role type and separate create fields display(separate display only)
  --}}
 
 
