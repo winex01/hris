5388ba223af8aec765eca7a89d9dbaa85e7893ab
winex01 <winnie131212592@gmail.com>
2020-12-14 23:49:54 +0800

    fix comment

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -1,6 +1,6 @@
 <!-- This file is used to store topbar (left) items -->
 
-<li class="nav-item px-3"><a class="nav-link" href="{{ backpack_url('dashboard') }}">@lang('lang.dashboard')</a></li>
+{{-- <li class="nav-item px-3"><a class="nav-link" href="{{ backpack_url('dashboard') }}">@lang('lang.dashboard')</a></li> --}}
 
 @if (config('web-tinker.enabled'))
 	<li class="nav-item px-3">
