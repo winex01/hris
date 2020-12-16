c8c38f6bd4e085cf17c76101f0da95f51323593d
winex01 <winnie131212592@gmail.com>
2020-12-16 12:34:16 +0800

    done load menu from db

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,78 +1,66 @@
 <x-rename-breadcrumbs></x-rename-breadcrumbs>
 
 {{-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 --}}
-
-{{-- <li class="nav-item">
-	<a class="nav-link" href="{{ backpack_url('dashboard') }}">
-		<i class="la la-home nav-icon"></i> @lang('lang.dashboard')
-	</a>
-</li> --}}
-
 @php
-	
-
-	$menus = \App\Models\Menu::all();
-
-	// dd($menus);
-
+	$menus = \App\Models\Menu::whereNull('parent_id')->orderBy('lft')->get();
 @endphp
 
+@foreach ($menus as $menu)
+	@if ($menu->url == 'audittrail')
+		@can('admin_view')
+			<li class="nav-title">
+				@lang('lang.admin_only')
+			</li>
+		@endcan
+	@endif
 
-{{-- Users, Roles, Permissions --}}
-{{-- @canany(['user_list', 'role_list', 'permission_list'])
-	<li class="nav-item nav-dropdown">
-		<a class="nav-link nav-dropdown-toggle" href="#">
-			<i class="nav-icon la la-users"></i> 
-			@lang('lang.authentication')
-		</a>
+	@if ($menu->url != null) 
+		@can($menu->permission)
+			<li class="nav-item">
+				<a class="nav-link" href="{{ backpack_url($menu->url) }}">
+					{!! $menu->icon !!} 
+					{{ $menu->label }} 
+				</a>
+			</li>
+		@endcan
+	@else
+		@php
+			$subMenus = \App\Models\Menu::where('parent_id', $menu->id)->orderBy('lft')->get();
+			$subMenusPermissions = $subMenus->pluck('permission')->toArray();
+		@endphp
+		
+		{{-- submenu --}}
+		@foreach ($subMenus as $subMenu)
+			@if ($loop->first && auth()->user()->can($subMenusPermissions))
+					<li class="nav-item nav-dropdown">
+						<a class="nav-link nav-dropdown-toggle" href="#">
+							{!! $menu->icon !!} 
+							{{ $menu->label }} 
+						</a>
+						<ul class="nav-dropdown-items">
+			@endif
+							@can('user_list')
+							 	<li class="nav-item">
+							 		<a class="nav-link" href="{{ backpack_url($subMenu->url) }}">
+							 			{!! $subMenu->icon !!} 
+										{{ $subMenu->label }} 
+							 		</a>
+						 		</li>
+							@endcan
 
-		<ul class="nav-dropdown-items">
-
-			@can('user_list')
-			 	<li class="nav-item">
-			 		<a class="nav-link" href="{{ backpack_url('user') }}">
-			 			<i class="nav-icon la la-user"></i> 
-			 			<span>@lang('lang.users')</span>
-			 		</a>
-		 		</li>
-			@endcan
-
-			@can('role_list')
-			  	<li class="nav-item">
-			  		<a class="nav-link" href="{{ backpack_url('role') }}">
-			  			<i class="nav-icon la la-id-badge"></i> 
-			  			<span>@lang('lang.roles')</span>
-			  		</a>
-			  	</li>
-			@endcan
-
-			@can('permission_list')
-				<li class="nav-item">
-					<a class="nav-link" href="{{ backpack_url('permission') }}">
-						<i class="nav-icon la la-key"></i> 
-						<span>@lang('lang.permissions')</span>
-					</a>
-				</li>
-			@endcan
-
-		</ul>
-	</li>
-@endcanany --}}
-
-
-{{-- @can('admin_view')
-	<li class="nav-title">
-		@lang('lang.admin_only')
-	</li>
-@endcan --}}
-
+			@if ($loop->last && auth()->user()->can($subMenusPermissions))
+						</ul>
+					</li>
+			@endif
+		@endforeach
+	@endif
+@endforeach
 
 
 
 @php
-	// TODO:: select menu from DB
-	// TODO:: refactor and clean
-    // TODO:: generate seeder
+	// TODO:: refactor and clean lang
+    // TODO:: generate seeder for menu
 	// TODO:: app settings seeders
 	// TODO:: schedule to run auto backup
 	// TODO:: add theme
