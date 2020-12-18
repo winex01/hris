bae93939ab3889c74b6e8e52ee3577962723ffef
winex01 <winnie131212592@gmail.com>
2020-12-18 10:20:00 +0800

    add sibling migration todo comments

--- database/migrations/2020_12_18_021640_create_siblings_table.php
+++ database/migrations/2020_12_18_021640_create_siblings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChildrensTable extends Migration
+class CreateSiblingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,26 @@ class CreateChildrensTable extends Migration
      */
     public function up()
     {
-        Schema::create('childrens', function (Blueprint $table) {
+        Schema::create('siblings', function (Blueprint $table) {
             $table->id();
              $table->foreignId('employee_id')
             ->constrained()
             ->onDelete('cascade')
             ->onUpdate('cascade');
             
-            $table->string('child_name')->nullable(); # Enter the name of child.
-            $table->date('birth_date')->nullable(); # Enter the birth date of child.
-            $table->string('birth_place')->nullable(); # Enter the birth place of child.
+
+            // TODO::
+            // siblings - string - Enter the name of sibling.
+            // birth date - date - Enter the birth of sibling.
+            // birth place - string - Enter the birth place of sibling.
+            // occupation - string - Enter the occupation of sibling.
+            // company - enter the company of sibling.
+
+
 
             // $table->string('attachment')->nullable();
             $table->timestamps();
             $table->softDeletes();
-
-            // TODO:: create crud
         });
     }
 
@@ -39,6 +43,6 @@ class CreateChildrensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('childrens');
+        Schema::dropIfExists('siblings');
     }
 }
