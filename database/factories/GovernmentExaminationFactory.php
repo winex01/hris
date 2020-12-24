a2021ea476a5a0bdc316d268ede9fbf7c5d60e63
winex01 <winnie131212592@gmail.com>
2020-12-24 10:58:57 +0800

    fix gov exam factory

---
+++ database/factories/GovernmentExaminationFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\GovernmentExamination;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GovernmentExaminationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = GovernmentExamination::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'employee_id'  => function (){
+                return \App\Models\Employee::factory()->create()->id;
+            },
+            'title'       => $this->faker->title,
+            'institution' => $this->faker->sentence(2),
+            'date'        => $this->faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now'),
+            'venue'       => $this->faker->paragraph,
+            'rating'      => rand(70,100),
+        ];
+    }
+}
