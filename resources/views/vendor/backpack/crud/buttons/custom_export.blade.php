25f01246bdc3c8a98d01b44114ef7472f6e77a1f
winex01 <winnie131212592@gmail.com>
2020-12-28 00:42:25 +0800

    dont remove check items and dont reload after exporting

--- resources/views/vendor/backpack/crud/buttons/custom_export.blade.php
+++ resources/views/vendor/backpack/crud/buttons/custom_export.blade.php
@@ -137,8 +137,8 @@
 						}).show();
 					}
 
-				  	crud.checkedItems = [];
-				  	crud.table.ajax.reload();
+				  	// crud.checkedItems = [];
+				  	// crud.table.ajax.reload();
 				},
 				error: function(result) {
 					// Show an alert with the result
