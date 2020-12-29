a5fe89afe3ac2d7d2f32daa7fa7ab0340fd13055
winex01 <winnie131212592@gmail.com>
2020-12-29 17:19:35 +0800

    fix backpack dir included

--- config/backup.php
+++ config/backup.php
@@ -34,7 +34,8 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path(),
+                    base_path('storage'),
+                    base_path('.env'),
                 ],
 
                 /*
