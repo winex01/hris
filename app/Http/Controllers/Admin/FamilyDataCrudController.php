edaa31094e045a4534da2734dc953ce1cf1c21cd
winex01 <winnie131212592@gmail.com>
2020-12-31 11:06:56 +0800

    add employee column orderable in family data crud

--- app/Http/Controllers/Admin/FamilyDataCrudController.php
+++ app/Http/Controllers/Admin/FamilyDataCrudController.php
@@ -43,6 +43,31 @@ class FamilyDataCrudController extends CrudController
      */
     protected function setupListOperation()
     {
+        $currentTable = $this->crud->model->getTable();
+
+        $this->crud->addColumn([
+            'name'     => 'employee',
+            'label'    => 'Employee'.trans('lang.unsearchable_column'),
+            'type'     => 'closure',
+            'function' => function($entry) {
+                return $entry->personable->full_name_with_badge;
+            },
+            'wrapper'  => [
+            'href'     => function ($crud, $column, $entry, $related_key) {
+                    $crud = str_replace('App\Models\\', '', $entry->personable_type);
+                    $crud = strtolower($crud);
+                    return backpack_url($crud.'?id='.$entry->personable_id);
+                },
+            ],
+            'orderable' => true,
+            'orderLogic' => function ($query, $column, $column_direction) use ($currentTable) {
+                return $query->join('employees', 'employees.id', '=', $currentTable.'.personable_id')
+                    ->orderBy('employees.last_name', $column_direction)
+                    ->orderBy('employees.first_name', $column_direction)
+                    ->orderBy('employees.middle_name', $column_direction)
+                    ->orderBy('employees.badge_id', $column_direction);
+            } 
+        ]);
         $this->showColumns();
     }
 
