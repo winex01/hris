9a1d587479bd6e3afa7d7104353b0c04763cc4f3
winex01 <winnie131212592@gmail.com>
2021-01-06 05:10:49 +0800

    make employee filter globa to all those have employee filter on list

--- app/Http/Controllers/Admin/Traits/CrudExtendTrait.php
+++ app/Http/Controllers/Admin/Traits/CrudExtendTrait.php
@@ -35,7 +35,7 @@ trait CrudExtendTrait
         if (method_exists($this->crud->model, 'employee')) {
             $this->crud->addFilter([
                     'name'  => 'employee',
-                    'type'  => 'select2',
+                    'type'  => 'custom_employee_filter',
                     'label' => 'Select Employee',
                 ],
                 function () {
@@ -52,6 +52,17 @@ trait CrudExtendTrait
                     $this->crud->addClause('where', 'employee_id', $value);
                 }
             );
+
+            // 
+            if (session('employee_filter') == null) {
+                session()->put('employee_filter', []);
+            }
+
+            $currentRoute = \Str::slug($this->crud->getRoute());
+            if (!in_array($currentRoute, session('employee_filter'))) {
+                session()->push('employee_filter', $currentRoute);
+            }
+
         }//end if
     }
 
