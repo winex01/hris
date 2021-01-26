9c7eb0be18725a63767f7e7e4af15fa5ed6ca424
winex01 <winnie131212592@gmail.com>
2021-01-26 19:08:55 +0800

    fix comment

--- app/Http/Controllers/Admin/EmploymentInformationCrudController.php
+++ app/Http/Controllers/Admin/EmploymentInformationCrudController.php
@@ -355,12 +355,11 @@ class EmploymentInformationCrudController extends CrudController
     /*
     |--------------------------------------------------------------------------
     | Fetch Inline Create Data
-    | NOTE:: I intentionaly ucfirst all function after the word fetch
-    | to match entity from crud. 
-    | eg: bec. if i name the function like this fetchPayBasis it would produce 
-    | fetch/pay-basis, since i dont want to alter soo much in custom_inline_create.blade.php
-    | to fix it i use lowercase to transform route fetch, ex. fetchPaybasis = fetch/paybasis
-    | which is match to entity crud of pay basis.
+    | NOTE:: I intentionaly ucfirst all function after the word fetch to match entity from 
+    | crud bec. if i name the function like this fetchPayBasis it would produce 
+    | fetch/pay-basis, since i dont want to alter too much in custom_inline_create.blade.php
+    | to fix it, i use lowercase to transform route fetch, ex. fetchPaybasis = fetch/paybasis
+    | which match to entity crud of pay basis. 
     |--------------------------------------------------------------------------
     */
     public function fetchCompany()
