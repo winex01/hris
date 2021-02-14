74680bbb6c903c98cf567b141d9cbaaa411be6a7
winex01 <winnie131212592@gmail.com>
2021-02-14 18:32:16 +0800

    fix validation for shift schedule working hours json start and en

--- app/Http/Requests/ShiftScheduleCreateRequest.php
+++ app/Http/Requests/ShiftScheduleCreateRequest.php
@@ -21,9 +21,38 @@ class ShiftScheduleCreateRequest extends FormRequest
         $rules = parent::rules();
 
         $append = [
-        	'working_hours' => 'required',
+            'working_hours' => 'required',
         ];
 
+        // if json wh is empty then override it to null to activate validation
+        if (request()->working_hours == '[{}]') {
+            request()->merge([
+                'working_hours' => null,
+            ]);
+        }else {
+            // 
+            $workingHours = json_decode(request()->working_hours);
+
+            foreach ($workingHours ?? [] as $wh) {
+                if (!property_exists($wh, 'start') || !property_exists($wh, 'end')) {
+                    $append['start_end_field'] = 'required';
+                }
+            }
+        }
+
+        // TODO:: validation for overtime hours must have both start and end
+
         return collect($rules)->merge($append)->toArray();
+    }
+
+    public function messages()
+    {
+        $msg = parent::messages();
+
+        $append = [
+            'start_end_field.required' => 'The start and end field of working hours is required.',
+        ];
+
+        return collect($msg)->merge($append)->toArray();
     }
 }
