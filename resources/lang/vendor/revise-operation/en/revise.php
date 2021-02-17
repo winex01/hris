15d04e25a145424462328cd72513b2f2b5b4f255
winex01 <winnie131212592@gmail.com>
2021-02-17 19:17:33 +0800

    override revise label

---
+++ resources/lang/vendor/revise-operation/en/revise.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack Revise Operation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the CRUD interface.
+    | You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    // Revisions
+    'revisions'            => '',
+];
