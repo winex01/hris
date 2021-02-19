9192b3a94fa9605e28ebc98a0c61e4ba77cc7abb
winex01 <winnie131212592@gmail.com>
2021-02-20 03:21:03 +0800

    change label for null string in revision

--- app/Models/Traits/RevisionableInitTrait.php
+++ app/Models/Traits/RevisionableInitTrait.php
@@ -10,7 +10,7 @@ trait RevisionableInitTrait
     //protected $historyLimit = 500; //Maintain a maximum of 500 changes at any point of time, while cleaning up old revisions.
 
 	protected $revisionCreationsEnabled = true;
-	protected $revisionNullString = 'nothing';
+	protected $revisionNullString = '';
 	protected $revisionUnknownString = 'unknown';
 
 	protected $revisionFormattedFields = [
