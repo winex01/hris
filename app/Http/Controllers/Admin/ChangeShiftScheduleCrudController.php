69cf59b2957ba8ef93da98c1abf9abeec93fabdb
winex01 <winnie131212592@gmail.com>
2021-03-01 13:00:37 +0800

    refactor controller and fix menus seeder

--- app/Http/Controllers/Admin/ChangeShiftScheduleCrudController.php
+++ app/Http/Controllers/Admin/ChangeShiftScheduleCrudController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\ChangeShiftScheduleRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Illuminate\Support\Str;
 
 /**
  * Class ChangeShiftScheduleCrudController
