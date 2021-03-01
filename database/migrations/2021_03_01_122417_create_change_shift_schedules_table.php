5369a2b88565b887f7226c315f2844b9e586b029
winex01 <winnie131212592@gmail.com>
2021-03-01 12:30:52 +0800

    add change_shift_schedules migration

---
+++ database/migrations/2021_03_01_122417_create_change_shift_schedules_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChangeShiftSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('change_shift_schedules', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('employee_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            // date
+            $table->date('date');
+
+            // shift schedule id
+            $table->foreignId('shift_schedule_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('change_shift_schedules');
+    }
+}
