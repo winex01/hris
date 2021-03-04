f5631d64845dbd04c62fa69ccc843fb7966368ee
winex01 <winnie131212592@gmail.com>
2021-03-04 23:59:17 +0800

    refactor and transfer to calendar operation modal

--- app/Http/Controllers/Admin/Operations/CalendarOperation.php
+++ app/Http/Controllers/Admin/Operations/CalendarOperation.php
@@ -63,6 +63,9 @@ trait CalendarOperation
         // var is use in crud/inc/custom_printData.blade.php
         $this->data['contentClass'] = 'col-md-12';
 
+        // modals
+        $this->data['modalLists'] = $this->modalLists();
+
         // load the view
         return view("crud::custom_calendar_view", $this->data);
     }
@@ -244,5 +247,30 @@ trait CalendarOperation
             }
         }
         return $events;
+    }
+
+    private function modalLists()
+    {
+        return [
+            '<div class="modal fade" id="changeShiftScheduleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>'
+        ];
     }
 }
