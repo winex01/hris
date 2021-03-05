b33b2834057a0f8cb4d90a737a3f432f481d839b
winex01 <winnie131212592@gmail.com>
2021-03-06 05:04:04 +0800

    refactor calendar operation remove modalists

--- app/Http/Controllers/Admin/Operations/CalendarOperation.php
+++ app/Http/Controllers/Admin/Operations/CalendarOperation.php
@@ -282,34 +282,6 @@ trait CalendarOperation
 
     private function modalLists()
     {
-        return [
-            '<div class="modal fade" id="changeShiftScheduleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Change Shift Schedule</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body bg-light">
-
-                  <div class="card">
-                    <div class="card-body row">
-                    
-
-
-                    </div>
-                  </div>
-
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary">Save</button>
-                  </div>
-                </div>
-              </div>
-            </div>'
-        ];
+        return [];
     }
 }
