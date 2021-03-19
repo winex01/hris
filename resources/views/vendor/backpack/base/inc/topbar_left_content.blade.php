6e7074cc34bfa044d6cc62f94759a18857a12077
winex01 <winnie131212592@gmail.com>
2021-03-19 12:32:39 +0800

    fix todo comment and boiler plate at nav button clock

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -1,16 +1,15 @@
 <!-- This file is used to store topbar (left) items -->
 
-{{-- 
-<li class="nav-item px-3"><a class="nav-link" href="#">Dashboard</a></li>
-<li class="nav-item px-3"><a class="nav-link" href="#">Users</a></li>
---}}
-
-{{-- TODO:: clock --}}
 <li class="nav-item px-3 ml-2"><a class="nav-link text-white" href="#">{{ date('h : i A') }}</a></li> 
 
 <li class="nav-item px-3 ml-n4">
-	<a class="btn btn-info btn-sm" href="#"><i class="las la-clock"></i> IN &nbsp; &nbsp;</a>
-	<a class="btn btn-warning btn-sm ml-1" href="#"><i class="las la-stopwatch"></i> OUT</a>
-	<a class="btn btn-danger btn-sm ml-1" href="#"><i class="las la-hourglass-start"></i> BREAK START</a>
-	<a class="btn btn-danger btn-sm ml-1" href="#"><i class="las la-hourglass-end"></i> BREAK END</a>
+	<button class="btn btn-info btn-sm"><i class="las la-clock"></i> IN &nbsp; &nbsp;</button>
+	<button class="btn btn-secondary btn-sm ml-1"><i class="las la-stopwatch"></i> OUT</button>
+	{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button> --}}
+	{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button> --}}
 </li>
+
+@php
+	// TODO:: if employee current date shift schedule has dynamic break show Break start and break END
+	// TODO:: if IN is enable OUT and others disable, if IN is disabled OUT and others is enable
+@endphp
