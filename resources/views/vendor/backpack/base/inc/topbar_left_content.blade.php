05b4242202673142b08f8994e6657a03426dcd44
winex01 <winnie131212592@gmail.com>
2021-03-19 14:52:07 +0800

    remove hover cursor in date time clock

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -1,7 +1,7 @@
 <!-- This file is used to store topbar (left) items -->
 
 <li class="nav-item px-3 ml-2"><a class="nav-link text-white" href="#">
-	<span class="btn btn-outline-secondary" id="clock" title="{{ __('Server Time') }}">{{ date('d. F  Y - g : i A') }}</span>
+	<span style="cursor:auto;" class="btn btn-outline-secondary" id="clock" title="{{ __('Server Time') }}">{{ date('d. F  Y - g : i A') }}</span>
 </a></li> 
 
 <li class="nav-item px-3 ml-n4">
