12cb67be7bb2319b09083b4eab5a8ccfcccf8bcd
winex01 <winnie131212592@gmail.com>
2021-03-19 12:15:11 +0800

    wip nav time and button clock in and break

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -1,9 +1,16 @@
 <!-- This file is used to store topbar (left) items -->
 
-@can('admin_web_tinker')
-	<li class="nav-item px-3">
-		<a href="{{ url(config('web-tinker.path')) }}" target="_blank" class="nav-link" href="#">
-			{{ __('Tinker') }}
-		</a>
-	</li>
-@endcan
+{{-- 
+<li class="nav-item px-3"><a class="nav-link" href="#">Dashboard</a></li>
+<li class="nav-item px-3"><a class="nav-link" href="#">Users</a></li>
+--}}
+
+{{-- TODO:: clock --}}
+<li class="nav-item px-3 ml-2"><a class="nav-link text-white" href="#">{{ date('h : i A') }}</a></li> 
+
+<li class="nav-item px-3 ml-n4">
+	<a class="btn btn-info btn-sm" href="#"><i class="las la-clock"></i> IN &nbsp; &nbsp;</a>
+	<a class="btn btn-warning btn-sm ml-1" href="#"><i class="las la-stopwatch"></i> OUT</a>
+	<a class="btn btn-danger btn-sm ml-1" href="#"><i class="las la-hourglass-start"></i> BREAK START</a>
+	<a class="btn btn-danger btn-sm ml-1" href="#"><i class="las la-hourglass-end"></i> BREAK END</a>
+</li>
