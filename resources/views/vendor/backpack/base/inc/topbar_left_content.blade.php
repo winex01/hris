7529d25e170ef4bb50d08540ecb39530a025537e
winex01 <winnie131212592@gmail.com>
2021-03-21 11:02:58 +0800

    show only time logs in out button and etc. if user has employee link

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -4,15 +4,16 @@
 	<span style="cursor:auto;" class="btn btn-outline-secondary" id="clock" title="{{ __('Server Time') }}">{{ date('d. F  Y - g : i : s A') }}</span>
 </a></li> 
 
-<li class="nav-item px-3 ml-n4">
-	<button class="btn btn-info btn-sm"><i class="las la-clock"></i> IN &nbsp; &nbsp;</button>
-	<button class="btn btn-secondary btn-sm ml-1"><i class="las la-stopwatch"></i> OUT</button>
-	{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button> --}}
-	{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button> --}}
-</li>
+@if (auth()->user()->employee_id)
+	<li class="nav-item px-3 ml-n4">
+		<button class="btn btn-info btn-sm"><i class="las la-clock"></i> IN &nbsp; &nbsp;</button>
+		<button class="btn btn-secondary btn-sm ml-1"><i class="las la-stopwatch"></i> OUT</button>
+		{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button> --}}
+		{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button> --}}
+	</li>
+@endif
 
 @php
-	// TODO:: create clock retrieved from backend and animete using jquery setInterval
 	// TODO:: if employee current date shift schedule has dynamic break show Break start and break END
 	// TODO:: if IN is enable OUT and others disable, if IN is disabled OUT and others is enable
 @endphp
