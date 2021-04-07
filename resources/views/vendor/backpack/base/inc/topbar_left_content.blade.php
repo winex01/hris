56d18e956c9823f9080a2c4e14402bd5d95c8f8b
winex01 <winnie131212592@gmail.com>
2021-04-08 01:10:24 +0800

    show only break in and break out if shift today is dynamic break = true

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -1,12 +1,19 @@
 <!-- This file is used to store topbar (left) items -->
 <x-clock></x-clock>
 
-@if (auth()->user()->employee_id)
+@php
+	$employee = auth()->user()->employee;
+@endphp
+
+@if ($employee)
 	<li class="nav-item px-3 ml-n4">
 		<button class="btn btn-info btn-sm"><i class="las la-clock"></i> IN &nbsp; &nbsp;</button>
 		<button class="btn btn-secondary btn-sm ml-1"><i class="las la-stopwatch"></i> OUT</button>
-		{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button> --}}
-		{{-- <button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button> --}}
+
+		@if ($employee->shiftToday()->dynamic_break)
+			<button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button>
+			<button class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button>
+		@endif
 	</li>
 @endif
 
