9c2b8b02f08a0ae8b8e1ed6629caebd72227f262
winex01 <winnie131212592@gmail.com>
2021-04-12 06:59:11 +0800

    fix if statement if if no shift tody object data

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -5,7 +5,7 @@
 	$employee = auth()->user()->employee;
 	
 	// total array count multiply by 2 bec. it is pairs
-	$totalAcceptableLogs = count($employee->shiftToday()->wh) * 2; 
+	// $totalAcceptableLogs = count($employee->shiftToday()->wh) * 2; 
 @endphp
 
 @if ($employee)
@@ -13,7 +13,7 @@
 		<button id="timeIn" class="btn btn-info btn-sm"><i class="las la-clock"></i> IN &nbsp; &nbsp;</button>
 		<button id="timeOut" class="btn btn-secondary btn-sm ml-1"><i class="las la-stopwatch"></i> OUT</button>
 
-		@if ($employee->shiftToday()->dynamic_break)
+		@if ($employee->shiftToday() && $employee->shiftToday()->dynamic_break)
 			<button id="timeBreakStart" class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-start"></i> BREAK START</button>
 			<button id="timeBreakEnd" class="btn btn-danger btn-sm ml-1"><i class="las la-hourglass-end"></i> BREAK END</button>
 		@endif
