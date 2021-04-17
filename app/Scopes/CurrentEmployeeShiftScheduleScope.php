13426de87a4ee2cdec857d533dce0c3b518b1b97
winex01 <winnie131212592@gmail.com>
2021-04-18 07:28:49 +0800

    fix globe scope of employee shift schedule 'CurrentEmployeeShiftScheduleScope'

--- app/Scopes/CurrentEmployeeShiftScheduleScope.php
+++ app/Scopes/CurrentEmployeeShiftScheduleScope.php
@@ -19,14 +19,24 @@ class CurrentEmployeeShiftScheduleScope implements Scope
     {
         $builder->whereRaw('(
                 employee_shift_schedules.employee_id,
+                employee_shift_schedules.effectivity_date,
                 employee_shift_schedules.created_at
             ) = ANY(
                 SELECT 
                     t2.employee_id,
-                    max(t2.created_at)
+                    t2.effectivity_date,
+                    MAX(t2.created_at)
                 FROM employee_shift_schedules t2
                 WHERE t2.effectivity_date <= ?
-                GROUP BY t2.employee_id
-        )', currentDate());
+                AND t2.effectivity_date = (
+                    SELECT MAX(t3.effectivity_date) FROM employee_shift_schedules t3 
+                    WHERE t3.employee_id = t2.employee_id 
+                    AND t3.effectivity_date <= ?
+                )
+                GROUP BY t2.employee_id, t2.effectivity_date
+        )', [
+            currentDate(),
+            currentDate()
+        ]);
     }
 }
