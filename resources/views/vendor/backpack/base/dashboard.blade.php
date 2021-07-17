f01371b68b64b65bbb01b7ff7db8d7d8dac2e94b
winex01 <winnie131212592@gmail.com>
2021-07-17 09:00:20 +0800

    fix dashboard html template

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -27,44 +27,6 @@
                   ',
               ]
             ],
-            [
-              'type' => 'card',
-              // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-              // 'class' => 'card bg-dark text-white', // optional
-              'content' => [
-                  'header' => 'Another card title', // optional
-                  'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-              ]
-            ],
-            [
-              'type' => 'card',
-              // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-              // 'class' => 'card bg-dark text-white', // optional
-              'content' => [
-                  'header' => 'Yet another card title', // optional
-                  'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-              ]
-            ],
-
-              [
-              'type' => 'card',
-              // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-              // 'class' => 'card bg-dark text-white', // optional
-              'content' => [
-                  'header' => 'Lorem Ipsum', // optional
-                  'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-              ]
-            ],
-
-              [
-              'type' => 'card',
-              // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-              // 'class' => 'card bg-dark text-white', // optional
-              'content' => [
-                  'header' => 'Test Card Title', // optional
-                  'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-              ]
-            ],
       ]
     ];
 
