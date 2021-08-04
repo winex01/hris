b4a63b1307534a45d01d4a43ae1badb83a25e638
winex01 <winnie131212592@gmail.com>
2021-08-04 13:40:53 +0800

    fix user with permission of emp time clock

--- resources/views/components/clock.blade.php
+++ resources/views/components/clock.blade.php
@@ -35,75 +35,77 @@
 </script>
 
 <script type="text/javascript">
-$('.clock').click(function() {
-	$.ajax({
-		url: '{{ route('employeetimeclock.show') }}',
-		type: 'POST',
- 		success: function (data) {
-			console.log(data);
-			if (data.show) {
-				var buttonIn = '';
-				var buttonOut = '';
-				var buttonBreakStart = '';
-				var buttonBreakEnd = '';
-				var shift = `<p class="text-danger">{!! trans('lang.clock_no_shift_desc') !!}</p>`;
+@if (auth()->user()->can('employee_time_clock_show'))
+	$('.clock').click(function() {
+		$.ajax({
+			url: '{{ route('employeetimeclock.show') }}',
+			type: 'POST',
+	 		success: function (data) {
+				console.log(data);
+				if (data.show) {
+					var buttonIn = '';
+					var buttonOut = '';
+					var buttonBreakStart = '';
+					var buttonBreakEnd = '';
+					var shift = `<p class="text-danger">{!! trans('lang.clock_no_shift_desc') !!}</p>`;
 
-				if (data.hasShift) {
-					shift = '';
+					if (data.hasShift) {
+						shift = '';
 
-					if (data.in) {
-						buttonIn = `<button value="1" class="mb-1 btn btn-info btn-sm empTimeClockLog"> {!! trans('lang.clock_button_in') !!} </button> `; 
+						if (data.in) {
+							buttonIn = `<button value="1" class="mb-1 btn btn-info btn-sm empTimeClockLog"> {!! trans('lang.clock_button_in') !!} </button> `; 
+						}
+
+						if (data.out) {
+							buttonOut = `<button value="2" class="mb-1 btn btn-danger btn-sm empTimeClockLog"> {!! trans('lang.clock_button_out') !!} </button> `;
+						}
+
+						if (data.breakStart) {
+							buttonBreakStart = `<button value="3" class="mb-1 btn btn-warning btn-sm empTimeClockLog"> {!! trans('lang.clock_button_break_start') !!} </button> `;
+						}
+
+						if (data.breakEnd) {
+							buttonBreakEnd = `<button value="4" class="mb-1 btn btn-success btn-sm empTimeClockLog"> {!! trans('lang.clock_button_break_end') !!} </button> `;
+						}
 					}
 
-					if (data.out) {
-						buttonOut = `<button value="2" class="mb-1 btn btn-danger btn-sm empTimeClockLog"> {!! trans('lang.clock_button_out') !!} </button> `;
-					}
+					Swal.fire({
+					    position: 'top',
+					    showConfirmButton: false,
+					    width: '300px',
+					    html: `<p> {!! trans('lang.clock_title') !!} </p>` + buttonIn + buttonOut + buttonBreakStart + buttonBreakEnd + shift,
+					    didOpen: function (dObj) {
+			                $('.empTimeClockLog').on('click',function () {
+	                   			
+	                   			$.ajax({
+				                   	url: '{{ route('employeetimeclock.loggedTime') }}',
+				                   	type: 'POST',
+				                   	data: {
+				                   		type : $(this).val()
+				                   	},
+				                   	success: function (data) {
+				                   		console.log(data);
+			                   			Swal.close();
+				                   	}
+			                   });
+			                   
+			                });
+			            },
+			            timer: 10000,
+			            timerProgressBar: true,
+			            willClose: true
+				  	}); // end swal
 
-					if (data.breakStart) {
-						buttonBreakStart = `<button value="3" class="mb-1 btn btn-warning btn-sm empTimeClockLog"> {!! trans('lang.clock_button_break_start') !!} </button> `;
-					}
-
-					if (data.breakEnd) {
-						buttonBreakEnd = `<button value="4" class="mb-1 btn btn-success btn-sm empTimeClockLog"> {!! trans('lang.clock_button_break_end') !!} </button> `;
-					}
-				}
-
-				Swal.fire({
-				    position: 'top',
-				    showConfirmButton: false,
-				    width: '300px',
-				    html: `<p> {!! trans('lang.clock_title') !!} </p>` + buttonIn + buttonOut + buttonBreakStart + buttonBreakEnd + shift,
-				    didOpen: function (dObj) {
-		                $('.empTimeClockLog').on('click',function () {
-                   			
-                   			$.ajax({
-			                   	url: '{{ route('employeetimeclock.loggedTime') }}',
-			                   	type: 'POST',
-			                   	data: {
-			                   		type : $(this).val()
-			                   	},
-			                   	success: function (data) {
-			                   		console.log(data);
-		                   			Swal.close();
-			                   	}
-		                   });
-		                   
-		                });
-		            },
-		            timer: 10000,
-		            timerProgressBar: true,
-		            willClose: true
-			  	}); // end swal
-
-			}// end if show
-		},// end success
-		statusCode: {
-            419: function() { 
-                window.location.href = '{{ route('backpack.auth.login') }}'; 
-            }
-        },
-	});// end ajax
-});// end click event
+				}// end if show
+			},// end success
+			statusCode: {
+	            419: function() { 
+	                window.location.href = '{{ route('backpack.auth.login') }}'; 
+	            }
+	        },
+		});// end ajax
+	});// end click event
+@endif
 </script>
 @endpush
 
