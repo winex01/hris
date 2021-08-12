ab4497e1af94502f2ddd75d4e1a6553a776d8755
winex01 <winnie131212592@gmail.com>
2021-08-12 15:17:49 +0800

    fix inline delete swal error

--- resources/views/vendor/backpack/crud/buttons/delete.blade.php
+++ resources/views/vendor/backpack/crud/buttons/delete.blade.php
@@ -16,73 +16,149 @@
 		// e.preventDefault();
 		var route = $(button).attr('data-route');
 
-		swal({
+		// swal({
+		//   title: "{!! trans('backpack::base.warning') !!}",
+		//   text: "{!! trans('backpack::crud.delete_confirm') !!}",
+		//   icon: "warning",
+		//   buttons: ["{!! trans('backpack::crud.cancel') !!}", "{!! trans('backpack::crud.delete') !!}"],
+		//   dangerMode: true,
+		// }).then((value) => {
+		// 	if (value) {
+		// 		$.ajax({
+		// 	      url: route,
+		// 	      type: 'DELETE',
+		// 	      success: function(result) {
+		// 	          if (result == 1) {
+		// 				  // Redraw the table
+		// 				  if (typeof crud != 'undefined' && typeof crud.table != 'undefined') {
+		// 					  crud.table.draw(false);
+		// 				  }
+
+		// 	          	  // Show a success notification bubble
+		// 	              new Noty({
+		//                     type: "success",
+		//                     text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
+		//                   }).show();
+
+		// 	              // Hide the modal, if any
+		// 	              $('.modal').modal('hide');
+		// 	          } else {
+		// 	              // if the result is an array, it means 
+		// 	              // we have notification bubbles to show
+		// 	          	  if (result instanceof Object) {
+		// 	          	  	// trigger one or more bubble notifications 
+		// 	          	  	Object.entries(result).forEach(function(entry, index) {
+		// 	          	  	  var type = entry[0];
+		// 	          	  	  entry[1].forEach(function(message, i) {
+		// 			          	  new Noty({
+		// 		                    type: type,
+		// 		                    text: message
+		// 		                  }).show();
+		// 	          	  	  });
+		// 	          	  	});
+		// 	          	  } else {// Show an error alert
+		// 		              swal({
+		// 		              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+	 //                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+		// 		              	icon: "error",
+		// 		              	timer: 4000,
+		// 		              	buttons: false,
+		// 		              });
+		// 	          	  }			          	  
+		// 	          }
+
+		// 	         	// winex: if operation is show then redirect
+		// 				@include('crud::inc.custom_redirect_to_crud_route')
+		// 	      },
+		// 	      error: function(result) {
+		// 	          // Show an alert with the result
+		// 	          swal({
+		//               	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+  //                       text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+		//               	icon: "error",
+		//               	timer: 4000,
+		//               	buttons: false,
+		//               });
+		// 	      }
+		// 	  }); // end ajax
+		// 	}
+		// });
+
+
+		Swal.fire({
 		  title: "{!! trans('backpack::base.warning') !!}",
+		  // text: "You won't be able to revert this!",
 		  text: "{!! trans('backpack::crud.delete_confirm') !!}",
-		  icon: "warning",
-		  buttons: ["{!! trans('backpack::crud.cancel') !!}", "{!! trans('backpack::crud.delete') !!}"],
-		  dangerMode: true,
-		}).then((value) => {
-			if (value) {
-				$.ajax({
-			      url: route,
-			      type: 'DELETE',
-			      success: function(result) {
-			          if (result == 1) {
-						  // Redraw the table
-						  if (typeof crud != 'undefined' && typeof crud.table != 'undefined') {
-							  crud.table.draw(false);
-						  }
+		  icon: 'warning',
+		  showCancelButton: true,
+		  cancelButtonColor: '#d33',
+		  confirmButtonColor: '#3085d6',
+		  confirmButtonText: "{!! trans('backpack::crud.delete') !!}",
+		  cancelButtonText: "{!! trans('backpack::crud.cancel') !!}",
+		  reverseButtons: true,
+		}).then((result) => {
+		  if (result.isConfirmed) {
+	  		$.ajax({
+		      url: route,
+		      type: 'DELETE',
+		      success: function(result) {
+		          if (result == 1) {
+					  // Redraw the table
+					  if (typeof crud != 'undefined' && typeof crud.table != 'undefined') {
+						  crud.table.draw(false);
+					  }
 
-			          	  // Show a success notification bubble
-			              new Noty({
-		                    type: "success",
-		                    text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
-		                  }).show();
+		          	  // Show a success notification bubble
+		              new Noty({
+	                    type: "success",
+	                    text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
+	                  }).show();
 
-			              // Hide the modal, if any
-			              $('.modal').modal('hide');
-			          } else {
-			              // if the result is an array, it means 
-			              // we have notification bubbles to show
-			          	  if (result instanceof Object) {
-			          	  	// trigger one or more bubble notifications 
-			          	  	Object.entries(result).forEach(function(entry, index) {
-			          	  	  var type = entry[0];
-			          	  	  entry[1].forEach(function(message, i) {
-					          	  new Noty({
-				                    type: type,
-				                    text: message
-				                  }).show();
-			          	  	  });
-			          	  	});
-			          	  } else {// Show an error alert
-				              swal({
-				              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
-	                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
-				              	icon: "error",
-				              	timer: 4000,
-				              	buttons: false,
-				              });
-			          	  }			          	  
-			          }
+		              // Hide the modal, if any
+		              $('.modal').modal('hide');
+		          } else {
+		              // if the result is an array, it means 
+		              // we have notification bubbles to show
+		          	  if (result instanceof Object) {
+		          	  	// trigger one or more bubble notifications 
+		          	  	Object.entries(result).forEach(function(entry, index) {
+		          	  	  var type = entry[0];
+		          	  	  entry[1].forEach(function(message, i) {
+				          	  new Noty({
+			                    type: type,
+			                    text: message
+			                  }).show();
+		          	  	  });
+		          	  	});
+		          	  } else {// Show an error alert
+			              Swal.fire({
+			              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+			              	icon: "error",
+			              	timer: 4000,
+			              	showConfirmButton: false,
+			              });
+		          	  }			          	  
+		          }
 
-			         	// winex: if operation is show then redirect
-						@include('crud::inc.custom_redirect_to_crud_route')
-			      },
-			      error: function(result) {
-			          // Show an alert with the result
-			          swal({
-		              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
-                        text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
-		              	icon: "error",
-		              	timer: 4000,
-		              	buttons: false,
-		              });
-			      }
-			  });
-			}
-		});
+		         	// winex: if operation is show then redirect
+					@include('crud::inc.custom_redirect_to_crud_route')
+		      },
+		      error: function(result) {
+		          // Show an alert with the result
+		          Swal.fire({
+	              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+                    text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+	              	icon: "error",
+	              	timer: 4000,
+	              	showConfirmButton: false,
+	              });
+		      }
+		  	}); // end ajax
+
+		  }
+		});//end swal
+
 
       }
 	}
