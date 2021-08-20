7139af48613226b24608502ee851266696250cb7
winex01 <winnie131212592@gmail.com>
2021-08-21 05:40:42 +0800

    fix Withholding tax basis request validation

--- app/Http/Requests/PayrollPeriodCreateRequest.php
+++ app/Http/Requests/PayrollPeriodCreateRequest.php
@@ -21,16 +21,16 @@ class PayrollPeriodCreateRequest extends FormRequest
         $rules = parent::rules();
 
         $addRules = [
-            'description'       => 'nullable|min:5',
-            'year_month'        => 'required|min:7|max:7',
-            'payroll_start'     => 'required|date',
-            'payroll_end'       => 'required|date',
-            'deduct_pagibig'    => 'required|boolean',
-            'deduct_philhealth' => 'required|boolean',
-            'deduct_sss'        => 'required|boolean',
-            // TODO:: witholding tax
-            'grouping_id'       => 'required|numeric',
-            'is_last_pay'       => 'required|boolean',
+            'description'              => 'nullable|min:5',
+            'year_month'               => 'required|min:7|max:7',
+            'payroll_start'            => 'required|date',
+            'payroll_end'              => 'required|date',
+            'deduct_pagibig'           => 'required|boolean',
+            'deduct_philhealth'        => 'required|boolean',
+            'deduct_sss'               => 'required|boolean',
+            'withholding_tax_basis_id' => 'required|numeric',
+            'grouping_id'              => 'required|numeric',
+            'is_last_pay'              => 'required|boolean',
         ];
 
         $rules = array_merge($rules, $addRules);
