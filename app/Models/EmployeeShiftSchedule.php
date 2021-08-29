fe29e7ddec72d0c6c68a451c772a1d25bbc3357a
winex01 <winnie131212592@gmail.com>
2021-08-30 00:37:58 +0800

    refactor and reused employee shift schedule scope

--- app/Models/EmployeeShiftSchedule.php
+++ app/Models/EmployeeShiftSchedule.php
@@ -31,27 +31,7 @@ class EmployeeShiftSchedule extends Model
     {
         $temp =  openPayrollDetails();
         static::addGlobalScope('CurrentEmployeeShiftScheduleScope', function (Builder $builder) use($temp) {
-            $builder->whereRaw('(
-                    employee_shift_schedules.employee_id,
-                    employee_shift_schedules.effectivity_date,
-                    employee_shift_schedules.created_at
-                ) = ANY(
-                    SELECT 
-                        t2.employee_id,
-                        t2.effectivity_date,
-                        MAX(t2.created_at)
-                    FROM employee_shift_schedules t2
-                    WHERE t2.effectivity_date <= ?
-                    AND t2.effectivity_date = (
-                        SELECT MAX(t3.effectivity_date) FROM employee_shift_schedules t3 
-                        WHERE t3.employee_id = t2.employee_id 
-                        AND t3.effectivity_date <= ?
-                    )
-                    GROUP BY t2.employee_id, t2.effectivity_date
-            )', [
-                currentDate(),
-                currentDate()
-            ]);
+            (new self)->scopeDate($builder, currentDate());
         });
     }
 
@@ -112,7 +92,6 @@ class EmployeeShiftSchedule extends Model
     */
     public function scopeDate($query, $date)
     {
-    // NOTE:: whereRaw query is the same with the global scope
         return $query->withoutGlobalScope('CurrentEmployeeShiftScheduleScope')
             ->whereRaw('(
                     employee_shift_schedules.employee_id,
@@ -136,7 +115,6 @@ class EmployeeShiftSchedule extends Model
                 $date
             ]);
     }
-    // TODO:: fix this local scope and anonymouse global scope bec. its the same. refactor it
     /*
     |--------------------------------------------------------------------------
     | ACCESSORS
