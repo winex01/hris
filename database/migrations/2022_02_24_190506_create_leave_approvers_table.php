b5548e12a0335021c9c5305767066b4e50900b1b
winex01 <winnie131212592@gmail.com>
2022-02-24 19:12:30 +0800

    add leave approver migration

---
+++ database/migrations/2022_02_24_190506_create_leave_approvers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveApproversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_approvers', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('employee_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->onUpdate('cascade'); 
+
+            $table->json('approvers');
+            $table->date('effectivity_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_approvers');
+    }
+}
