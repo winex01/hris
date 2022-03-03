c5df3baca30ec59406a2575a4666bb681612141c
winex01 <winnie131212592@gmail.com>
2022-03-04 00:57:37 +0800

    show error if dependent table row is deleted

--- resources/views/vendor/backpack/crud/buttons/custom_force_delete.blade.php
+++ resources/views/vendor/backpack/crud/buttons/custom_force_delete.blade.php
@@ -1,8 +1,6 @@
 @if ($crud->hasAccess('forceDelete'))
-	<a href="javascript:void(0)" onclick="forceDeleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey().'/forceDelete') }}" class="btn btn-sm btn-link text-danger" data-button-type="delete" data-toggle="tooltip" title="{!! trans('backpack::crud.force_delete') !!}"><i class="la la-trash"></i></a>
+	<a href="javascript:void(0)" onclick="forceDeleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey().'/forceDelete') }}" class="btn btn-sm btn-link text-danger" data-button-type="forceDeleteEntry" data-toggle="tooltip" title="{!! trans('backpack::crud.force_delete') !!}"><i class="la la-trash"></i></a>
 @endif
-
-{{-- @dump(url($crud->route)) --}}
 
 {{-- Button Javascript --}}
 {{-- - used right away in AJAX operations (ex: List) --}}
@@ -11,14 +9,12 @@
 <script>
 
 	if (typeof forceDeleteEntry != 'function') {
-	  $("[data-button-type=delete]").unbind('click');
+	  $("[data-button-type=forceDeleteEntry]").unbind('click');
 
 	  function forceDeleteEntry(button) {
 		// ask for confirmation before deleting an item
 		// e.preventDefault();
-		var button = $(button);
-		var route = button.attr('data-route');
-		var row = $("#crudTable a[data-route='"+route+"']").closest('tr');
+		var route = $(button).attr('data-route');
 
 		const swalWithBootstrapButtons = Swal.mixin({
 		  customClass: {
@@ -38,73 +34,68 @@
 		  cancelButtonText: "{!! trans('backpack::crud.cancel') !!}",
 		  reverseButtons: true,
 		}).then((value) => {
-			if (value.isConfirmed) {
-				$.ajax({
-			      url: route,
-			      type: 'DELETE',
-			      success: function(result) {
-			          if (result == 1) {
-						// Redraw the table
-						if (typeof crud != 'undefined' && typeof crud.table != 'undefined') {
-							crud.table.draw(false);
-						}
-			          	  // Show a success notification bubble
-			              new Noty({
-		                    type: "success",
-		                    text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
-		                  }).show();
+		  if (value.isConfirmed) {
+	  		$.ajax({
+		      url: route,
+		      type: 'DELETE',
+		      success: function(result) {
+		          if (result == 1) {
+					  // Redraw the table
+					  if (typeof crud != 'undefined' && typeof crud.table != 'undefined') {
+						  crud.table.draw(false);
+					  }
 
-			              // Hide the modal, if any
-			              $('.modal').modal('hide');
+		          	  // Show a success notification bubble
+		              new Noty({
+	                    type: "success",
+	                    text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
+	                  }).show();
 
-			              // Remove the details row, if it is open
-			              if (row.hasClass("shown")) {
-			                  row.next().remove();
-			              }
+		              // Hide the modal, if any
+		              $('.modal').modal('hide');
+		          } else {
+		              // if the result is an array, it means 
+		              // we have notification bubbles to show
+		          	  if (result instanceof Object) {
+		          	  	// trigger one or more bubble notifications 
+		          	  	Object.entries(result).forEach(function(entry, index) {
+		          	  	  var type = entry[0];
+		          	  	  entry[1].forEach(function(message, i) {
+				          	  new Noty({
+			                    type: type,
+			                    text: message
+			                  }).show();
+		          	  	  });
+		          	  	});
+		          	  } else {// Show an error alert
+			              Swal.fire({
+			              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+			              	icon: "error",
+			              	timer: 4000,
+			              	showConfirmButton: false,
+			              });
+		          	  }			          	  
+		          }
 
-			              // Remove the row from the datatable
-			              row.remove();
-			          } else {
-			              // if the result is an array, it means 
-			              // we have notification bubbles to show
-			          	  if (result instanceof Object) {
-			          	  	// trigger one or more bubble notifications 
-			          	  	Object.entries(result).forEach(function(entry, index) {
-			          	  	  var type = entry[0];
-			          	  	  entry[1].forEach(function(message, i) {
-					          	  new Noty({
-				                    type: type,
-				                    text: message
-				                  }).show();
-			          	  	  });
-			          	  	});
-			          	  } else {// Show an error alert
-				              swal({
-				              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
-	                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
-				              	icon: "error",
-				              	timer: 4000,
-				              	buttons: false,
-				              });
-			          	  }			          	  
-			          }
+		         	// winex: if operation is show then redirect
+					@include('crud::inc.custom_redirect_to_crud_route')
+		      },
+		      error: function(result) {
+		          // Show an alert with the result
+		          Swal.fire({
+	              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+                    text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+	              	icon: "error",
+	              	timer: 4000,
+	              	showConfirmButton: false,
+	              });
+		      }
+		  	}); // end ajax
 
-			          // if operation is show then redirect
-			          @include('crud::inc.custom_redirect_to_crud_route')
-			      },
-			      error: function(result) {
-			          // Show an alert with the result
-			          swal({
-		              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
-                        text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
-		              	icon: "error",
-		              	timer: 4000,
-		              	buttons: false,
-		              });
-			      }
-			  });
-			}
-		});
+		  }
+		});//end swal
+
 
       }
 	}
@@ -113,4 +104,3 @@
 	// crud.addFunctionToDataTablesDrawEventQueue('forceDeleteEntry');
 </script>
 @if (!request()->ajax()) @endpush @endif
-
