855e6d2de89aaea268d5ac8b473d36dfaa648c98
winex01 <winnie131212592@gmail.com>
2022-03-03 18:10:43 +0800

    fix bulkbuttons showing in history or trashed filter active

--- resources/views/vendor/backpack/crud/filters/custom_simple_hide_bottom_buttons.blade.php
+++ resources/views/vendor/backpack/crud/filters/custom_simple_hide_bottom_buttons.blade.php
@@ -4,13 +4,17 @@
 	<script type="text/javascript">
 		{{-- hide bottom buttons if history filter is active --}}
 		$("li[filter-type]").click(function(e){	
-		    var filterType = $(this).attr('filter-type');
 
-		    if (filterType == 'custom_simple_hide_bottom_buttons' && $("li[filter-type]").hasClass('active')) {
+		    if ($("li[filter-type='custom_simple_hide_bottom_buttons']").hasClass('active')) {
+
     			$('.bulk-button').hide();
+
 		    }else {
+
 	    		$('.bulk-button').show();
+
 		    }
+
 		});
 
 		{{-- if Remove filters is click --}}
@@ -19,3 +23,4 @@
 		});
 	</script>
 @endpush
+{{-- TODO:: fix this bulk buttons showing and not showing sometimes --}}
