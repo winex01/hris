e39fc0b7ae16d35992372023d6cf762d6602ef28
winex01 <winnie131212592@gmail.com>
2022-03-15 02:37:08 +0800

    fix breadcrumbs show home instead of HRIS

--- resources/views/vendor/backpack/base/inc/breadcrumbs.blade.php
+++ resources/views/vendor/backpack/base/inc/breadcrumbs.blade.php
@@ -3,7 +3,7 @@
 	  <ol class="breadcrumb bg-transparent p-0 {{ config('backpack.base.html_direction') == 'rtl' ? 'justify-content-start' : 'justify-content-end' }}">
 	  	@foreach ($breadcrumbs as $label => $link)
 	  		@php
-	  			$label = ($label == 'Admin') ? config('app.name') : $label;
+	  			$label = ($label == 'Admin') ? 'Home' : $label;
 	  		@endphp
 	  		@if ($link)
 			    <li class="breadcrumb-item text-capitalize"><a href="{{ $link }}">{{ $label }}</a></li>
