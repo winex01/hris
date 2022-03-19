037333afe8652b77a2010b4d682674a8fea1eae2
winex01 <winnie131212592@gmail.com>
2022-03-19 09:19:52 +0800

    add command to generate employment information and app settings seeeders

--- app/Console/Commands/GenerateEmploymentInfo.php
+++ app/Console/Commands/GenerateEmploymentInfo.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\EmploymentInformation;
 use Illuminate\Console\Command;
 
 class GenerateEmploymentInfo extends Command
@@ -20,6 +21,8 @@ class GenerateEmploymentInfo extends Command
      */
     protected $description = 'Generate faker employment information data.';
 
+    private $faker;
+
     /**
      * Create a new command instance.
      *
@@ -28,6 +31,8 @@ class GenerateEmploymentInfo extends Command
     public function __construct()
     {
         parent::__construct();
+    
+        $this->faker = \Faker\Factory::create();
     }
 
     /**
@@ -55,7 +60,43 @@ class GenerateEmploymentInfo extends Command
 
         $this->getOutput()->progressStart(count($employeeChoice));
         foreach ($employeeChoice as $employee) {
-            sleep(1);
+            $employeeId = modelInstance('Employee')
+                ->where('badge_id', getStringBetweenParenthesis($employee))
+                ->pluck('id')
+                ->first();
+
+            $empInfoFields = classInstance('EmploymentInfoField')->pluck('field_type', 'name')->toArray();
+
+
+            foreach ($empInfoFields as $fieldName => $type) {
+                if ($type == 1) { //select ID
+                    $class = strtolower($fieldName);
+                    $class = convertToClassName($class);
+                
+                    $id = modelInstance($class)::select('id')->inRandomOrder()->first()->id;
+
+                    modelInstance('EmploymentInformation')->create([
+                        'employee_id' => $employeeId,
+                        'field_name' => $fieldName,
+                        'field_value' => '{ "id": "'.$id.'" }',
+                        'effectivity_date' => currentDate(),
+                    ]);
+                }else {
+                    // decimal
+
+                    $amount = $this->faker->numberBetween(15, 99) . "000";
+                    $amount = (int)$amount;
+
+                    modelInstance('EmploymentInformation')->create([
+                        'employee_id' => $employeeId,
+                        'field_name' => $fieldName,
+                        'field_value' => json_encode($amount),
+                        'effectivity_date' => currentDate(),
+                    ]);
+                }
+            }
+
+
             $this->getOutput()->progressAdvance();
         }
 
---
+++ database/factories/LevelFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Level;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LevelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Level::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => ucwords($this->faker->word),
+        ];
+    }
+}
